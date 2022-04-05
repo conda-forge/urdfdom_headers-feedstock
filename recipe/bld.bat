00f8a48b1a4443dd48512447d9c168605e7bdcfd
Silvio Traversaro <silvio@traversaro.it>
2022-04-05 18:24:25 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,7 @@ cmake ^
     -G "NMake Makefiles" ^
     -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -D CMAKE_BUILD_TYPE=Release ^
+    -DAPPEND_PROJECT_NAME_TO_INCLUDEDIR:BOOL=OFF ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 nmake install
